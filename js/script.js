10b7ee9bc6317e0f9193b57d170ebe0aec4611b3
indirakirani08-source <indirakirani08@gmail.com>
2025-11-08 01:17:37 +0700

    Implement code changes to enhance functionality and improve performance

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-welcomeMessage();
+// welcomeMessage();
 
 function welcomeMessage() {
     let name = prompt("Please enter your name:");
     if (name != null) {
-    document.getElementById('welcome-speech').innerText = 'Hello, ' + name + '!';
+    document.getElementById('welcome-speech').innerText = 'Halo, ' + name + ' Selamat Datang di Mie Gacoan, Rasanya Jagoan!';
     }
 }
